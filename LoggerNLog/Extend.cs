b25412f1b3ffea48e8e481d351a06424d5a744da
曹一梁 <caoyiliang@me.com>
2023-08-22 11:08:12 +0800

    更新依赖

---
+++ LoggerNLog/Extend.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text;
+using LogInterface;
+
+namespace LoggerNLog
+{
+    public static class Extend
+    {
+        public static IServiceCollection UseNLog(this IServiceCollection services) => services.AddSingleton<ILogFactory, NLogFactory>();
+    }
+}
